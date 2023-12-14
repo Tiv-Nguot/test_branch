3eb5c2f772656d911317c163b68abf26136b026e
TIV.NGUOT <TIV.NGUOT@PNC.LAN>
2023-12-14 07:17:29 +0700

    #2 add log

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
 let txt = "Here";
 console.log('txt', txt);
-let number = 5;
-console.log('number', number);
-console.log(1);
-console.log(1);
-console.log(1);
-console.log(1);
-console.log(1);
-
-console.log(5);
+console.log("test");
